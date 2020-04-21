2e31dca2a60bf0b05ae22a1ded73c445cfe0d141
Shmuli <shmuli9@gmail.com>
2020-04-21 18:57:42 +0100

    spun off a pure version of the algo, seperate from flask infrastructure

---
+++ pure/solver.py
@@ -0,0 +1,83 @@
+import time
+
+from wordtree import WordTree, WTNode
+
+from pure.config import Config
+
+moves = {"right", "left", "up", "down", "upRight", "upLeft", "downRight", "downLeft"}
+
+global wt
+wt = WordTree()
+
+
+def generate_valid_words(board):
+    valid_words = []
+    # print(board)
+
+    boardSize = 4
+    for i in range(boardSize):
+        for j in range(boardSize):
+            start_node = board.nodes[i][j]
+            if start_node.letter in wt.children:
+                valid_words += rec_words(start_node, wt.children[start_node.letter], visited={})
+
+    # withD = len(valid_words)
+    # print(sorted(valid_words))
+    # withOD = len(set(valid_words))
+    # diff = withD - withOD
+    # print(f"{diff},{diff / withD * 100: .2f}, {withD}")
+    # print("Number of duplicates", diff)
+    # print(f"%age duplicates {diff / withD * 100: .2f}%")
+    return valid_words
+
+
+def rec_words(node, wt_node: WTNode, word="", visited=None):
+    valid_words = []
+    curr_word = word + wt_node.data
+
+    if wt_node.isWord:  # word has been found in wordtree matching the boggle word
+        valid_words.append(curr_word)
+        wt_node.isWord = False  # mark as non word to prevent duplication
+        wt.voided_words.append(wt_node)
+        void_this_node = True
+        for child in wt_node.children:
+            if not wt_node.children[child].void:
+                void_this_node = False
+                break
+        if void_this_node:
+            wt_node.void = True  # no unvoided children so void
+            wt.voided_nodes.append(wt_node)  # save to unvoid for reuse of wordtree dictionary
+    legal_trans = node.possible_transitions()
+
+    for move in moves.intersection(legal_trans):  # loop through all legal moves
+        next_node = node.transitions[move]
+        if next_node not in visited:
+            next_letter = next_node.letter
+            if next_letter in wt_node.children:
+                next_wt_node = wt_node.children[next_letter]
+                if not next_wt_node.void:
+                    new_visited = set(visited)  # copy values to new variable
+                    new_visited.add(node)  # add current node to visited
+                    valid_words.extend(
+                        rec_words(next_node, next_wt_node, curr_word, new_visited))
+
+    # check if wt node has unvoided children, if it does not, then void the node
+    for child in wt_node.children:
+        if not wt_node.children[child].void:
+            return valid_words  # early return, as one or more children are not void
+    wt_node.void = True  # no unvoided children so void
+    wt.voided_nodes.append(wt_node)
+    return valid_words
+
+
+def build_word_dictionary(min_word_size=3):
+    start = time.time()
+    # wt = WordTree()
+
+    with open(Config.DICTIONARY_ADDRESS, encoding="utf8") as file:
+        for word in file.read().split("\n"):
+            wt.add(word)
+    end = time.time()
+    print(f"took {end - start:.6f}s to create the WordTree")
+
+    return wt
---
+++ pure/tests.py
@@ -0,0 +1,110 @@
+import time
+
+from pure.board import Board
+from pure.solver import build_word_dictionary, generate_valid_words
+
+if __name__ == '__main__':
+
+    global wt
+    wt = build_word_dictionary()
+
+
+    def run_generator(board):
+        start = time.time()
+        word_list = generate_valid_words(board)
+        end = time.time()
+
+        print(f"{len(word_list)} words were generated in {end - start:.6f} seconds")
+        return sorted(word_list)
+
+
+    def verify_algo(strict=False):
+        if not strict:
+            print("\nPreconfigured board:")
+            print("-" * 20)
+        output = run_generator(Board("LOPGPOCIHBIEGKLS"))
+
+        expected_output = ['BICE', 'BICES', 'BILE', 'BILES', 'BILK', 'BIO', 'BIOPIC', 'BIS', 'BISE', 'BOH', 'BOI',
+                           'BOIL', 'BOILS',
+                           'BOIS', 'BOLO', 'BOO', 'BOOL', 'BOP', 'CEIL', 'CEILS', 'CEL', 'CELS', 'CIBOL', 'CIEL',
+                           'CIELS', 'CIG',
+                           'CIS', 'COB', 'COBLE', 'COBLES', 'COIL', 'COILS', 'COL', 'COLOBI', 'COO', 'COOL', 'COOP',
+                           'COP',
+                           'COPIES', 'ECO', 'EIK', 'ELK', 'ELS', 'GIE', 'GIES', 'GIP', 'HOB', 'HOC', 'HOI', 'HOIK',
+                           'HOIS',
+                           'HOISE', 'HOLO', 'HOLP', 'HOO', 'HOOP', 'HOP', 'ICE', 'ICES', 'ILK', 'ISLE', 'KIS', 'LEI',
+                           'LEIS',
+                           'LES', 'LIB', 'LICE', 'LIE', 'LIES', 'LIS', 'LOB', 'LOBI', 'LOCI', 'LOCIE', 'LOCIES',
+                           'LOCIS', 'LOCO',
+                           'LOO', 'LOOBIES', 'LOOIE', 'LOOIES', 'LOOP', 'LOP', 'OBI', 'OBIS', 'OIK', 'OIL', 'OILS',
+                           'OIS', 'OOH',
+                           'OOP', 'PHO', 'PHOBIC', 'PIC', 'PICE', 'PIE', 'PIES', 'PIG', 'PLOP', 'POCO', 'POH', 'POI',
+                           'POIS',
+                           'POISE', 'POL', 'POLO', 'POO', 'POOH', 'POOL', 'POOP', 'POP', 'SEC', 'SECO', 'SEI', 'SEIK',
+                           'SEIL',
+                           'SEL', 'SIB', 'SIC', 'SICE', 'SIK', 'SILE', 'SILK', 'SLICE']
+
+        if not strict:
+            # run_generator(Board("QuEENPOCIHBIEGKLS"))
+
+            print("\nRandom boards:")
+            print("-" * 20)
+            for _ in range(1):
+                run_generator(Board())
+
+        if output == expected_output:
+            return True
+        return False
+
+
+    def test_suite(num_runs=10):
+
+        def avg(numbers):
+            total = 0
+            for el in numbers:
+                total += el
+            return total / len(numbers)
+
+        dice = ["QuEENPOCIHBIEGKLS", "LOPGPOCIHBIEGKLS"]
+        #  dice[0] if run % 2 == 0 else dice[1]
+
+        run_times = []
+        global duplicates
+        for run in range(num_runs):
+            wt.resetTree()
+            # d = dice[0] if run % 2 == 0 else dice[1]
+            d = ""
+            start = time.time()
+            word_list = generate_valid_words(Board(d))
+            end = time.time()
+            del word_list
+            run_times.append(end - start)
+            del start, end
+
+        precision = 6
+        print(
+            f"average run time over {num_runs} random boards was: {avg(run_times):.{precision}f}s (total:{sum(run_times):.{precision}f}s)")
+        print(f"max run time was {max(run_times):.{precision}f}s")
+        print(f"min run time was {min(run_times)}s")
+        print(f"delta (max - min) run time was {max(run_times) - min(run_times):.{precision}f}s")
+        print(run_times[:1000])
+
+
+    if not verify_algo(True):
+        print("❌ - Algo failed to run successfully on Preconfigured board ")
+    else:
+        pass
+        print("Algo verified, running test suite")
+        test_suite(10000)
+
+
+def checkWordTree(wordList, wordTree):
+    print("checking wordtree")
+    missed_words = []
+    for word in wordList:
+        if not wordTree.findString(word):
+            missed_words.append(word)
+    print("finished checking wordtree")
+    print(len(missed_words), " words were skipped:\n", missed_words)
+
+# checkWordTree(words, wt)
