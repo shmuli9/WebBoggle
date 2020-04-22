b90cc758371af3b010f19f665a3109e4729342a8
Shmuli <shmuli9@gmail.com>
2020-04-22 17:50:01 +0100

    changed recursive wordtree creation to simple loop use sets to store voided nodes added build_wordtree to WordTree class

--- app/wordtree.py
+++ app/wordtree.py
@@ -4,59 +4,91 @@ from app.config import Config
 
 
 class WTNode:
-    def __init__(self, string, children: {}):
+    def __init__(self, string):
         self.data = string
         self.isWord = False
         self.void = False
         self.children = {}
-        if children:
-            self.children[children.data] = children
-
-    # prints the node and all its children in a string
-    def __str__(self):
-        st = "(" + str(self.data) + ", " + str(self.isWord) + ") -> {"
-        if self.children is not None:
-            st += str(self.children)
-        return st + "}"
 
 
 class WordTree:
     def __init__(self):
         self.children = {}
-        self.voided_nodes = []
-        self.voided_words = []
+        self.voided_nodes = set()
+        self.voided_words = set()
 
     def __str__(self):
         return str(self.children)
 
     def addToBranch(self, st, node):
-        if not st:  # base case, string is empty
-            node.isWord = True
-            return
+        i = 0
+        while i < len(st):
+            if i == len(st) - 1:
+                node.isWord = True
+                return
+            letter = st[i]
 
-        chars = st[0]
-        remaining = st[1:]
-        if chars == "Q":
-            chars = "Qu"
-            remaining = st[2:]
+            if letter == "Q":
+                letter = "Qu"
+                i += 1
+            i += 1
 
-        if chars not in node.children:
-            newNode = WTNode(chars, None)
-            node.children[chars] = newNode
-        return self.addToBranch(remaining, node.children[chars])
+            if letter not in node.children:
+                newNode = WTNode(letter)
+                node.children[letter] = newNode
+            node = node.children[letter]
+
+        # if not st:  # base case, string is empty
+        #     node.isWord = True
+        #     return
+        #
+        # chars = st[0]
+        # remaining = st[1:]
+        # if chars == "Q":
+        #     chars = "Qu"
+        #     remaining = st[2:]
+        #
+        # if chars not in node.children:
+        #     newNode = WTNode(chars, None)
+        #     node.children[chars] = newNode
+        # return self.addToBranch(remaining, node.children[chars])
 
     # adds the string st in the tree
     def add(self, st):
-        if len(st) > 2:
-            chars = st[0]
-            remaining = st[1:]
-            if chars == "Q":
-                chars = "Qu"
-                remaining = st[2:]
-            if chars not in self.children:
-                newNode = WTNode(chars, None)
-                self.children[chars] = newNode
-            self.addToBranch(remaining, self.children[chars])
+        l = len(st)
+        if l > 2:  # min word size is 3
+            i = 0
+            letter = st[0]
+            i += 1
+            if letter == "Q":
+                letter = "Qu"
+                i += 1
+            if letter not in self.children:
+                self.children[letter] = WTNode(letter)
+            node = self.children[letter]
+
+            while i < l:  # loop through remaining
+                letter = st[i]
+                if letter == "Q":
+                    letter = "Qu"
+                    i += 1
+                i += 1
+
+                if letter not in node.children:
+                    node.children[letter] = WTNode(letter)
+                node = node.children[letter]
+            node.isWord = True  # final node is a word
+
+        # if len(st) > 2:
+        #     chars = st[0]
+        #     remaining = st[1:]
+        #     if chars == "Q":
+        #         chars = "Qu"
+        #         remaining = st[2:]
+        #     if chars not in self.children:
+        #         newNode = WTNode(chars, None)
+        #         self.children[chars] = newNode
+        #     self.addToBranch(remaining, self.children[chars])
 
     def _findString(self, st, node):
         if not st:  # base case, string is empty or node is empty
@@ -121,26 +153,46 @@ class WordTree:
     def resetTree(self):
         for node in self.voided_nodes:
             node.void = False
-        self.voided_nodes = []
+        self.voided_nodes = set()
         for node in self.voided_words:
             node.isWord = True
-        self.voided_words = []
+        self.voided_words = set()
+
+    def build_wordtree(self, min_word_size=3):
+        start = time.time()
+
+        with open(Config.DICTIONARY_ADDRESS, encoding="utf8") as file:
+            for word in file.read().split("\n"):
+                l = len(word)
+                if l > min_word_size - 1:  # min word size is 3
+                    i = 0
+                    letter = word[0]
+                    i += 1
+                    if letter == "Q":
+                        letter = "Qu"
+                        i += 1
+                    if letter not in self.children:
+                        self.children[letter] = WTNode(letter)
+                    node = self.children[letter]
+
+                    while i < l:  # loop through remaining
+                        letter = word[i]
+                        if letter == "Q":
+                            letter = "Qu"
+                            i += 1
+                        i += 1
+
+                        if letter not in node.children:
+                            node.children[letter] = WTNode(letter)
+                        node = node.children[letter]
+                    node.isWord = True  # final node is a word
+        end = time.time()
+        print(f"took {end - start:.6f}s to create the WordTree")
+
+        return self
 
 
-def build_wordtree(min_word_size=3):
-    start = time.time()
-    wt = WordTree()
-
-    with open(Config.DICTIONARY_ADDRESS, encoding="utf8") as file:
-        for word in file.read().split("\n"):
-            wt.add(word)
-    end = time.time()
-    print(f"took {end - start:.6f}s to create the WordTree")
-
-    return wt
-
-
-wt = build_wordtree()
+wt = WordTree().build_wordtree()
 
 # wt = WordTree()
 # test_words = ["cart",
