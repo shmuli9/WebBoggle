8bb449b787e1b5a3656f007a39b5ae708fbe6646
Shmuli <shmuli9@gmail.com>
2020-05-01 00:26:19 +0100

    removed unnecessary findword function as unused in current implementation commented out old add method as no longer used, but is admittedly clearer, so good for reference refactored WordTree generation with comments

--- app/wordtree.py
+++ app/wordtree.py
@@ -20,102 +20,72 @@ class WordTree:
     def __str__(self):
         return str(self.children)
 
-    def addToBranch(self, st, node):
-        i = 0
-        while i < len(st):
-            if i == len(st) - 1:
-                node.isWord = True
-                return
-            letter = st[i]
-
-            if letter == "Q":
-                letter = "Qu"
-                i += 1
-            i += 1
-
-            if letter not in node.children:
-                newNode = WTNode(letter)
-                node.children[letter] = newNode
-            node = node.children[letter]
-
-        # if not st:  # base case, string is empty
-        #     node.isWord = True
-        #     return
-        #
-        # chars = st[0]
-        # remaining = st[1:]
-        # if chars == "Q":
-        #     chars = "Qu"
-        #     remaining = st[2:]
-        #
-        # if chars not in node.children:
-        #     newNode = WTNode(chars, None)
-        #     node.children[chars] = newNode
-        # return self.addToBranch(remaining, node.children[chars])
-
-    # adds the string st in the tree
-    def add(self, st):
-        l = len(st)
-        if l > 2:  # min word size is 3
-            i = 0
-            letter = st[0]
-            i += 1
-            if letter == "Q":
-                letter = "Qu"
-                i += 1
-            if letter not in self.children:
-                self.children[letter] = WTNode(letter)
-            node = self.children[letter]
-
-            while i < l:  # loop through remaining
-                letter = st[i]
-                if letter == "Q":
-                    letter = "Qu"
-                    i += 1
-                i += 1
-
-                if letter not in node.children:
-                    node.children[letter] = WTNode(letter)
-                node = node.children[letter]
-            node.isWord = True  # final node is a word
-
-        # if len(st) > 2:
-        #     chars = st[0]
-        #     remaining = st[1:]
-        #     if chars == "Q":
-        #         chars = "Qu"
-        #         remaining = st[2:]
-        #     if chars not in self.children:
-        #         newNode = WTNode(chars, None)
-        #         self.children[chars] = newNode
-        #     self.addToBranch(remaining, self.children[chars])
-
-    def _findString(self, st, node):
-        if not st:  # base case, string is empty or node is empty
-            if node:  # return multiplicity
-                return node.isWord
-            return False
-
-        chars = st[0]
-        remaining = st[1:]
-        if chars == "Q":
-            chars = "Qu"
-            remaining = st[2:]
-
-        if chars in node.children:
-            return self._findString(remaining, node.children[chars])
-        return False
-
-    def findString(self, st):
-        chars = st[0]
-        remaining = st[1:]
-        if chars == "Q":
-            chars = "Qu"
-            remaining = st[2:]
-
-        if chars not in self.children:  # or (chars in self.children and self.children[chars].void)
-            return False
-        return self._findString(remaining, self.children[chars])
+    # def addToBranch(self, st, node):
+    #     if not st:  # base case, string is empty
+    #         node.isWord = True
+    #         return
+    #
+    #     chars = st[0]
+    #     remaining = st[1:]
+    #     if chars == "Q":
+    #         chars = "Qu"
+    #         remaining = st[2:]
+    #
+    #     if chars not in node.children:
+    #         newNode = WTNode(chars, None)
+    #         node.children[chars] = newNode
+    #     return self.addToBranch(remaining, node.children[chars])
+    #
+    #
+    # def add(self, st):
+    #     '''
+    #     Adds the string st to the tree using recursive algorithm
+    #
+    #     :param st:
+    #     :return:
+    #     '''
+    #     if len(st) > 2:
+    #         chars = st[0]
+    #         remaining = st[1:]
+    #         if chars == "Q":
+    #             chars = "Qu"
+    #             remaining = st[2:]
+    #         if chars not in self.children:
+    #             newNode = WTNode(chars, None)
+    #             self.children[chars] = newNode
+    #         self.addToBranch(remaining, self.children[chars])
+    # def add(self, st):
+    #     """
+    #     Adds the string st to the tree
+    #     Originally used recursion, but more performance attained using while loop
+    #     See commented out code above...
+    #
+    #     :param st:
+    #     :return:
+    #     """
+    #     l = len(st)
+    #     if l > 2:  # min word size is 3
+    #         i = 0
+    #         letter = st[0]
+    #         i += 1
+    #         if letter == "Q":
+    #             letter = "Qu"
+    #             i += 1
+    #         if letter not in self.children:
+    #             self.children[letter] = WTNode(letter)
+    #         node = self.children[letter]
+    #
+    #         while i < l:  # loop through remaining
+    #             letter = st[i]
+    #             if letter == "Q":
+    #                 letter = "Qu"
+    #                 i += 1
+    #             i += 1
+    #
+    #             if letter not in node.children:
+    #                 node.children[letter] = WTNode(letter)
+    #             node = node.children[letter]
+    #         node.isWord = True  # final node is a word
 
     # Resets voided nodes and words (words that were set to isWord=False)
     def resetTree(self):
@@ -131,29 +101,30 @@ class WordTree:
 
         with open(Config.DICTIONARY_ADDRESS, encoding="utf8") as file:
             for word in file.read().split("\n"):
-                l = len(word)
-                if l > min_word_size - 1:  # min word size is 3
-                    i = 0
+                # originally self.add function was called here 290,000+ times per application run
+                # for perf, function code was added here
+                length = len(word)
+                if length > min_word_size - 1:
                     letter = word[0]
-                    i += 1
-                    if letter == "Q":
-                        letter = "Qu"
-                        i += 1
-                    if letter not in self.children:
-                        self.children[letter] = WTNode(letter)
-                    node = self.children[letter]
+                    letter_index = 1
+                    if letter == "Q":  # Q replacement, as QU is what appears on Boggle dice
+                        letter = "QU"
+                        letter_index += 1  # Boggle dictionary used as source, so all Q's followed by U
+                    if letter not in self.children:  # If no child (word) starts with current letter
+                        self.children[letter] = WTNode(letter)  # Add the node with value of letter to WordTree children
+                    node = self.children[letter]  # Initialise node to the correct WordTree child
 
-                    while i < l:  # loop through remaining
-                        letter = word[i]
+                    while letter_index < length:  # Loop through remaining letters
+                        letter = word[letter_index]  # Set letter to next char in word
                         if letter == "Q":
-                            letter = "Qu"
-                            i += 1
-                        i += 1
+                            letter = "QU"
+                            letter_index += 1
+                        letter_index += 1   # Increment letter_index (also used as loop counter)
 
-                        if letter not in node.children:
-                            node.children[letter] = WTNode(letter)
-                        node = node.children[letter]
-                    node.isWord = True  # final node is a word
+                        if letter not in node.children:  # If no child node exists for next letter
+                            node.children[letter] = WTNode(letter)  # Create new node with next letter
+                        node = node.children[letter]  # Move to next node
+                    node.isWord = True  # Set isWord, indicating end of word in WordTree
         end = time.time()
         print(f"took {end - start:.6f}s to create the WordTree")
 
@@ -161,27 +132,3 @@ class WordTree:
 
 
 wt = WordTree().build_wordtree()
-
-# wt = WordTree()
-# test_words = ["cart",
-#               "art",
-#               "car",
-#               "cccc",
-#               "Queen",
-#               "Que",
-#               "AQu"]
-# for word in test_words:
-#     wt.add(word)
-# print(wt)
-#
-# print(str(wt.findString("cart")))
-# wt.deleteWord("cart")
-# print(str(wt.findString("cart")))
-#
-# print(wt)
-#
-# for word in test_words:
-#     wt.deleteWord(word)
-#
-# print(wt)
-# print(str(wt.findString("AQu")))
