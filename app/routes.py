26718883f04bd53b0e9c5e0fffe0ff0139f7c579
Shmuli Margulies <shmuli9@gmail.com>
2021-01-04 14:24:54 +0000

    removed ssr templates
    Signed-off-by: Shmuli Margulies <shmuli9@gmail.com>

--- app/routes.py
+++ app/routes.py
@@ -11,9 +11,6 @@ from app.solver import solver
 bp = Blueprint("routes", __name__)
 
 
-
-
-
 @bp.route("/api/generate_board/", defaults={"game_id": ""}, methods=["POST"])
 @bp.route("/api/generate_board/<game_id>", methods=["POST"])
 def generate_board(game_id):
