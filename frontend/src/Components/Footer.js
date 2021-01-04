289f1d1b1c51d742d4abcafd47f74e9655415ea9
Shmuli Margulies <shmuli9@gmail.com>
2021-01-04 13:34:28 +0000

    cleaned up footer
    Signed-off-by: Shmuli Margulies <shmuli9@gmail.com>

--- frontend/src/Components/Footer.js
+++ frontend/src/Components/Footer.js
@@ -2,10 +2,10 @@ import React from "react";
 
 function Footer() {
     return (
-        <p className="w-100 text-center" style={{bottom: "0", height: "25px"}}>Copyright
-            <a href="https://github.com/shmuli9/WebBoggle"> WebBoggle</a> 2020 ©
-            <a href="https://github.com/shmuli9"> Shmuel Margulies</a>
-        </p>
+        <div className="w-100 text-center" style={{bottom: "0", height: "25px"}}>
+            Copyright <a href="https://github.com/shmuli9/WebBoggle">WebBoggle</a> 2020 © <a
+            href="https://github.com/shmuli9">Shmuel Margulies</a>
+        </div>
     )
 }
 
