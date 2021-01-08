281d4184810645d66ef12c0f23468ec6ed24b715
Shmuli Margulies <shmuli9@gmail.com>
2021-01-08 01:28:46 +0000

    minor changes to footer
    Signed-off-by: Shmuli Margulies <shmuli9@gmail.com>

--- frontend/src/Components/Footer.js
+++ frontend/src/Components/Footer.js
@@ -1,6 +1,6 @@
 function Footer() {
     return (
-        <div className="w-100 text-center" style={{bottom: "0", height: "25px"}}>
+        <div className="w-100 text-center my-3" style={{bottom: "0", height: "25px"}}>
             Copyright <a href="https://github.com/shmuli9/WebBoggle">WebBoggle</a> 2020 © <a
             href="https://github.com/shmuli9">Shmuel Margulies</a>
         </div>
